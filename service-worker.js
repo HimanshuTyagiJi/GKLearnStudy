fe54ca2e853e0592bfdd80b00c24d3a08bf7977d
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2024-11-16 19:13:07 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,46 @@
+// Cache Name
+const CACHE_NAME = 'pwa-cache-v1';
 
+// Files to Cache
+const CACHE_FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+// Install Event
 self.addEventListener('install', (event) => {
-    console.log('Service Worker Installed');
-    event.waitUntil(
-        caches.open('static-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/script.js',
-                '/path-to-icon/icon-192x192.png'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching Files...');
+      return cache.addAll(CACHE_FILES);
+    })
+  );
 });
 
+// Fetch Event
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing Old Cache...');
+            return caches.delete(cache);
+          }
         })
-    );
+      );
+    })
+  );
 });
