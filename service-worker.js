3032d99339b0f992b89ccae5bd04c03682d46b15
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2024-11-16 09:29:21 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,3 +7,18 @@ if ('serviceWorker' in navigator) {
         console.error('Service Worker registration failed:', error);
     });
 }
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
