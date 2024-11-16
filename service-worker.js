ec1dc53a836229e643b1baa40d3f6ce3e0aec3ab
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2024-11-16 18:22:04 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,4 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js') // सही पथ दें
-    .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(error => {
-        console.error('Service Worker registration failed:', error);
-    });
-}
-self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installed');
-});
 
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker: Activated');
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
 self.addEventListener('install', (event) => {
     console.log('Service Worker Installed');
     event.waitUntil(
