e7d94e274a19725043bdba8d33b09295caf26cb7
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2024-11-16 19:16:30 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,9 @@
-// Cache Name
-const CACHE_NAME = 'pwa-cache-v1';
-
-// Files to Cache
-const CACHE_FILES = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
-];
-
-// Install Event
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching Files...');
-      return cache.addAll(CACHE_FILES);
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js') // सही पथ दें
+    .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
     })
-  );
-});
-
-// Fetch Event
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Activate Event
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('Clearing Old Cache...');
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
+    .catch(error => {
+        console.error('Service Worker registration failed:', error);
+    });
+}
