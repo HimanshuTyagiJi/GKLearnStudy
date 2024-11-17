edbf38b61fb763b272f401c3468f5ebc02f958e7
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2024-11-17 09:10:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 
-// common.js
-(function() {
-  // Automatically add manifest link
-  const manifestLink = document.createElement('link');
-  manifestLink.rel = 'manifest';
-  manifestLink.href = '/manifest.json';
-  document.head.appendChild(manifestLink);
-
-  // Automatically register service worker
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/serviceworker.js', { scope: '/' })
-      .then(() => console.log("Service Worker Registered"))
-      .catch((err) => console.log("Service Worker Registration Failed", err));
-  }
-})();
 
 (function() {
     // Check if the current URL has .html
