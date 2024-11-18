3ecbb9ec0734b2f7ef75ee29363d8a5b4edee404
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2024-11-18 20:27:51 +0530

    Delete Ancient-Indian-History.js.js

--- Ancient-Indian-History.js.js
+++
@@ -1,6 +0,0 @@
-{
-                title: 'Good ',
-                options: ["A","B","C","D"],
-                correctOption: 2,
-                content: '<p><span class="color-content4">Explanation: </span>Option c</p>'
-            },
