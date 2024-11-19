46010b9325750ab62566e9d14fbfce72de1fb918
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2024-11-19 10:23:38 +0530

    Post deleted

--- Ancient-Indian-History.js
+++ Ancient-Indian-History.js
@@ -30,11 +30,7 @@ var sections = [
         correctOption: '0',
         content: '<p><span class="color-content4">Explanation: </span></p>'
     },
-{ title: '1',
-        options: ["","","",""],
-        correctOption: '0',
-        content: '<p><span class="color-content4">Explanation: </span></p>'
-    },
+
 { title: 'ho gaya done',
  options: ["","","",""],
  correctOption: '0',
