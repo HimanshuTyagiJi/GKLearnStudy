4ef8562a347e05a5e8110c125bc6e8a523019fc0
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2024-11-19 10:13:31 +0530

    New post added

--- Ancient-Indian-History.js
+++ Ancient-Indian-History.js
@@ -18,6 +18,11 @@ var sections = [
 
 
 
+{ title: '2',
+        options: ["","","",""],
+        correctOption: '0',
+        content: '<p><span class="color-content4">Explanation: </span></p>'
+    },
 { title: '1',
         options: ["","","",""],
         correctOption: '0',
