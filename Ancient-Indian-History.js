b69b81ba2668d18bc4370520559bfe67c36189b7
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2024-11-19 08:44:44 +0530

    New post added

--- Ancient-Indian-History.js
+++ Ancient-Indian-History.js
@@ -7,6 +7,12 @@ var paginationContainer = document.getElementById("pagination");
 var sections = [
     
   
+{
+        title: 'done',
+        options: ["","","",""],
+        correctOption: '0',
+        content: '<p><span class="color-content4">Explanation: </span></p>'
+    },
 { title: 'ho gaya done',
  options: ["","","",""],
  correctOption: '0',
