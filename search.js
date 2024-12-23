937359d8ab8270adaed031093b998dc51e685e1d
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2024-12-23 17:49:53 +0530

    Update search.js

--- search.js
+++ search.js
@@ -200,8 +200,8 @@ document.addEventListener("click", (event) => {
         const isWithinDistance = (element) => {
             const rect = element.getBoundingClientRect();
             return (
-                event.clientX > rect.left - 10 &&
-                event.clientX < rect.right + 10 
+                event.clientX > rect.left - 30 &&
+                event.clientX < rect.right + 30 
             );
         };
 
