24a3fabb01fd3d491004b7dcf5cc384f1104b822
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2024-12-28 15:55:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ window.onload = function() {
     });
 };
 
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Update service worker immediately
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => caches.delete(cache)) // Old cache clear
+            );
+        })
+    );
+});
 
 
 (function() {
