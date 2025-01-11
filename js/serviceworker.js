878decb85ae87e3901e989f946a07138cb56f123
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2025-01-11 17:21:36 +0530

    Update serviceworker.js

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -36,41 +36,8 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Fetch event (handling dynamic cache)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse; // Return cached response if available
-      }
-      return fetch(event.request).then((response) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          if (event.request.url.startsWith('http')) {
-            cache.put(event.request, response.clone()); // Cache the new response
-          }
-          return response;
-        });
-      });
-    })
-  );
-});
 
-// Push Notifications
-self.addEventListener('push', function(event) {
-  const options = {
-    body: event.data ? event.data.text() : 'Default notification body',
-    icon: 'https://gklearnstudy.in/GK-Learn-Study.png', // Your PWA icon
-    vibrate: [200, 100, 200],
-    actions: [
-      {action: 'explore', title: 'Go to site'},
-      {action: 'close', title: 'Close notification'},
-    ]
-  };
-  
-  event.waitUntil(
-    self.registration.showNotification('New Notification', options)
-  );
-});
+
 
 // Message event to clear cache
 self.addEventListener('message', (event) => {
@@ -81,5 +48,54 @@ self.addEventListener('message', (event) => {
       });
       console.log('All caches cleared.');
     });
+  }
+});
+/////////////////////////////////////////////////////////////////////
+
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
   }
 });
