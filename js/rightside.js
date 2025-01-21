2c7aef492d8a2b5f655db81e68f4edcd5cc94d09
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2025-01-21 20:15:56 +0530

    Update rightside.js

--- js/rightside.js
+++ js/rightside.js
@@ -23,29 +23,3 @@ function addLinks() {
 
 // Call the function to add links
 addLinks();
-
-
-
-function toggleMenu() {
-  var menuList = document.getElementById('menuList');
-  var menuToggle = document.querySelector('.menu-toggle');
-  if (menuList.style.display === 'none' || menuList.style.display === '') {
-      menuList.style.display = 'block';
-      menuToggle.style.display = 'none'; // Hide toggle button
-      document.addEventListener('click', closeMenuOnClickOutside);
-  } else {
-      menuList.style.display = 'none';
-      menuToggle.style.display = 'block'; // Show toggle button
-      document.removeEventListener('click', closeMenuOnClickOutside);
-  }
-}
-
-function closeMenuOnClickOutside(event) {
-  var menuList = document.getElementById('menuList');
-  var menuToggle = document.querySelector('.menu-toggle');
-  if (!menuList.contains(event.target) && event.target !== menuToggle) {
-      menuList.style.display = 'none';
-      menuToggle.style.display = 'block'; // Show toggle button
-      document.removeEventListener('click', closeMenuOnClickOutside);
-  }
-}
