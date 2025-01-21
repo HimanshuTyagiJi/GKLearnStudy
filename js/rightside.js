c1446dec18675ded506c150f9207a9bd096ab1fb
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2025-01-21 21:26:15 +0530

    Update rightside.js

--- js/rightside.js
+++ js/rightside.js
@@ -1,7 +1,4 @@
-// Get the link list element
 const linkList = document.getElementById('link-list');
-
-// Links data
 const links = [
   {name: "Mathematics All formulas", url: "https://gklearnstudy.in/mathematics-all-formulas"},
   {name: "Physics formulas", url: "https://gklearnstudy.in/physics-all-formulas"},
@@ -9,7 +6,7 @@ const links = [
   {name: "Periodic table", url: "https://gklearnstudy.in/all-formulas/periodic-table"},
 ];
 
-// Function to add links dynamically
+
 function addLinks() {
   links.forEach(link => {
     const li = document.createElement('li');
@@ -21,31 +18,31 @@ function addLinks() {
   });
 }
 
-// Call the function to add links
+
 addLinks();
 
-// Function to toggle menu
+
 function toggleMenu() {
   var menuList = document.getElementById('menuList');
   var menuToggle = document.querySelector('.menu-toggle');
   if (menuList.style.display === 'none' || menuList.style.display === '') {
     menuList.style.display = 'block';
-    menuToggle.style.display = 'none'; // Hide toggle button
+    menuToggle.style.display = 'none';
     document.addEventListener('click', closeMenuOnClickOutside);
   } else {
     menuList.style.display = 'none';
-    menuToggle.style.display = 'block'; // Show toggle button
+    menuToggle.style.display = 'block'; 
     document.removeEventListener('click', closeMenuOnClickOutside);
   }
 }
 
-// Function to close menu on click outside
+
 function closeMenuOnClickOutside(event) {
   var menuList = document.getElementById('menuList');
   var menuToggle = document.querySelector('.menu-toggle');
   if (!menuList.contains(event.target) && event.target !== menuToggle) {
     menuList.style.display = 'none';
-    menuToggle.style.display = 'block'; // Show toggle button
+    menuToggle.style.display = 'block'; 
     document.removeEventListener('click', closeMenuOnClickOutside);
   }
 }
