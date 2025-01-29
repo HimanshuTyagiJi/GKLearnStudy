7def1e66a54c37ac0b12c05d005fb7045f795f52
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2025-01-29 21:04:25 +0530

    Update rightside.js

--- js/rightside.js
+++ js/rightside.js
@@ -4,6 +4,14 @@ const links = [
   {name: "Physics formulas", url: "https://gklearnstudy.in/physics-all-formulas"},
   {name: "Chemical formulas", url: "https://gklearnstudy.in/all-formulas/all-chemical-formulas"},
   {name: "Periodic table", url: "https://gklearnstudy.in/all-formulas/periodic-table"},
+  {name: "Ancient Indian History", url: "https://gklearnstudy.in/gk-quiz/ancient-indian-history"},
+  {name: "Medieval Indian History", url: "https://gklearnstudy.in/gk-quiz/medieval-indian-history"},
+  {name: "Meodern Indian History", url: "https://gklearnstudy.in/gk-quiz/modern-indian-history"},
+  {name: "World History", url: "https://gklearnstudy.in/gk-quiz/world-history"},
+  {name: "Science & Technology", url: "https://gklearnstudy.in/gk-quiz/science-and-technology-mcq-quiz"},
+  {name: "Rivers & Lakes", url: "https://gklearnstudy.in/gk-quiz/rivers-and-lakes-mcq-quiz"},
+  {name: "Spacea & Universe", url: "https://gklearnstudy.in/gk-quiz/space-and-universe-mcq-quiz"},
+
 ];
 
 
