c99ecc348f4e1e4fffbb0e8e56379375e531b979
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2025-05-13 14:16:58 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -6,16 +6,6 @@ document.querySelectorAll('a').forEach(function(link) {
 
 
 
-// File: inject-adsense.js
-
-(function () {
-  const adsScript = document.createElement("script");
-  adsScript.setAttribute("async", "");
-  adsScript.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7067722696020503");
-  adsScript.setAttribute("crossorigin", "anonymous");
-
-  document.head.appendChild(adsScript);
-})();
 
 
 
