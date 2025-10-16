2bafd38d2426a97f8c8405a218e2c840573e0f48
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2025-10-16 20:57:34 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,79 @@
+
+const CACHE_NAME = 'gklearnstudy-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css/theme.css',
+  '/css/notification.css',
+  '/js/theme.js',
+  '/js/search-data.js',
+  '/js/comment.js',
+  '/js/notification.js',
+  '/js/search-data.json',
+  '/favicon.ico',
+  '/favicon.svg',
+  '/profile.html',
+  '/css/profile.css',
+  '/js/profile.js',
+  '/categories.html',
+  '/js/categories.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request because it's a stream and can only be consumed once.
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          response => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response because it's also a stream.
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
