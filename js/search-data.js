f19e143dc564c591c8c33b2c892540c097926322
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2025-10-22 14:45:20 +0530

    Update search-data.js

--- js/search-data.js
+++ js/search-data.js
@@ -111,7 +111,7 @@ function initializePostRendering(){
                     <button class="share-button" title="Share this page"><svg class="share-icon" viewBox="0 0 24 24" width="20" height="20"><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92s2.92-1.31 2.92-2.92-1.31-2.92-2.92-2.92z"></path></svg><span>Share</span></button>
                 </div>
             </div>`;
-        card.innerHTML=`<div class="card-thumbnail"><a href="/categories.html?category=${encodeURIComponent(post.category)}" class="category-badge">${post.category}</a>${readingTimeLabel}<a href="${post.url}" class="card-image-link" tabindex="-1">${imageHtml}</a></div><div class="card-content"><h3 class="card-title"><a href="${post.url}">${post.title}</a></h3>${summaryContent}</div>${metaBlock}`;
+        card.innerHTML=`<div class="card-thumbnail"><a href="/categories.html?category=${encodeURIComponent(post.category)}" class="category-badge">${post.category}</a>${readingTimeLabel}<a href="${post.url}" class="card-image-link" tabindex="-1" aria-label="${post.title.replace(/"/g, '&quot;')}">${imageHtml}</a></div><div class="card-content"><h3 class="card-title"><a href="${post.url}">${post.title}</a></h3>${summaryContent}</div>${metaBlock}`;
         return card
     };
     window.GKApp.createPostCard = createPostCard;
