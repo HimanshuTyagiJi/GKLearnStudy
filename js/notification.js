4734e08c4dbae651067f439bd420ef2a8f73a150
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2025-10-30 15:44:13 +0530

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -88,17 +88,32 @@ async function updateUIState() {
     if (isSubscribedOnThisPage) {
       notificationBtn.classList.add('subscribed');
       notificationBtn.title = 'You are subscribed. Click to unsubscribe.';
-      notificationBtn.innerHTML = '🔔 Unsubscribe';
+      notificationBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="24" height="24">
+  <path d="M18 8a6 6 0 0 0-12 0c0 7-3 9-3 9h18s-3-2-3-9"></path>
+  <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+</svg>
+';
     } else {
       notificationBtn.classList.remove('subscribed');
       notificationBtn.title = 'Click to subscribe for this page.';
-      notificationBtn.innerHTML = '🔕 Subscribe';
+      notificationBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="24" height="24">
+  <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+  <path d="M18 8a6 6 0 0 0-12 0c0 7-3 9-3 9h18s-3-2-3-9"></path>
+  <line x1="3" y1="3" x2="21" y2="21"></line>
+</svg>
+';
     }
   } else {
     isSubscribedOnThisPage = false;
     notificationBtn.classList.remove('subscribed');
     notificationBtn.title = 'Sign in and click to enable notifications.';
-    notificationBtn.innerHTML = '🔔 Subscribe';
+    notificationBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" 
+        fill="none" stroke="currentColor" stroke-width="2" 
+        stroke-linecap="round" stroke-linejoin="round" 
+        width="24" height="24">
+        <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+        <path d="M18 8a6 6 0 0 0-12 0c0 7-3 9-3 9h18s-3-2-3-9"></path>
+      </svg>';
   }
 }
 
